3a009572522fd2978d23f0f5439af3578d8432db
dishwasher-detergent <kenny_bass@outlook.com>
2024-09-03 11:01:22 -0500

    update icon

--- constants/EQUIPMENT_DETAILS.ts
+++ constants/EQUIPMENT_DETAILS.ts
@@ -5,7 +5,6 @@ import {
   LucideBoxes,
   LucideBuilding,
   LucideChurch,
-  LucideDollarSign,
   LucideDrill,
   LucideHospital,
   LucideHouse,
@@ -15,6 +14,7 @@ import {
   LucidePresentation,
   LucideRocket,
   LucideTentTree,
+  LucidePlaneTakeoff
 } from "lucide-react";
 
 export const EQUIPMENT_LIST: Record<string, Equipment> = {
@@ -35,7 +35,7 @@ export const EQUIPMENT_LIST: Record<string, Equipment> = {
     costMultiplier: 1.15,
     auPerSecond: 1,
     threshold: 50,
-    icon: LucideDollarSign,
+    icon: LucidePlaneTakeoff,
     upgrades: {
       seats: {
         name: "Additional Seats",
