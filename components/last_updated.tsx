764a6bf1e4c3b913a03fad4278d4e9c568247eb6
dishwasher-detergent <kenny_bass@outlook.com>
2024-09-03 16:58:50 -0500

    fix offline income to work when leaving browser tab

--- components/last_updated.tsx
+++ components/last_updated.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAtomValue, useSetAtom } from "jotai";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 
 import { autoIncrement } from "@/atoms/au";
@@ -10,47 +10,57 @@ import { equipment } from "@/atoms/equipment";
 import { EQUIPMENT_LIST } from "@/constants/EQUIPMENT_DETAILS";
 import { calculateUpgradeMultiplier } from "@/lib/utils";
 import { LOCALE } from "@/constants/GLOBAL";
+import { useAnimation } from "@/hooks/useAnimation";
 
 export function LastUpdated() {
   const last = useAtomValue(lastUpdated);
   const equip = useAtomValue(equipment);
   const update = useSetAtom(autoIncrement);
   const initialized = useRef(false);
+  const [delta, setDelta] = useState(0);
+
+  useAnimation((deltaTime) => {
+    setDelta((current) => current + deltaTime);
+  }, false);
 
   useEffect(() => {
-    if (!initialized.current) {
-      initialized.current = true;
-      const now = Date.now();
+    if (delta >= 1000) {
+      if (!initialized.current) {
+        initialized.current = true;
+        const now = Date.now();
 
-      if (last <= now - 60000) {
-        // 14400 is 4 hours in seconds
-        const diff = Math.min((now - last) / 1000, 14400);
-        update(diff);
-        let earned = 0;
+        if (last <= now - 60000) {
+          // 14400 is 4 hours in seconds
+          const diff = Math.min((now - last) / 1000, 14400);
+          update(diff);
+          let earned = 0;
 
-        Object.entries(equip).forEach(([key, eq]: any) => {
-          if (eq.value > 0) {
-            const item = EQUIPMENT_LIST[key];
-            const multiplier = calculateUpgradeMultiplier(eq, item);
+          Object.entries(equip).forEach(([key, eq]: any) => {
+            if (eq.value > 0) {
+              const item = EQUIPMENT_LIST[key];
+              const multiplier = calculateUpgradeMultiplier(eq, item);
 
-            earned += item.auPerSecond * multiplier * eq.value * diff;
+              earned += item.auPerSecond * multiplier * eq.value * diff;
+            }
+          });
+
+          if (earned > 0) {
+            setTimeout(() => {
+              toast.info(
+                "You've been away for a while, here's what you've earned while you were gone!",
+                {
+                  description: `You've earned ${earned.toLocaleString(LOCALE)} AUs while you were away!`,
+                  duration: 5000,
+                },
+              );
+            }, 500);
           }
-        });
-
-        if (earned > 0) {
-          setTimeout(() => {
-            toast.info(
-              "You've been away for a while, here's what you've earned while you were gone!",
-              {
-                description: `You've earned ${earned.toLocaleString(LOCALE)} AUs while you were away!`,
-                duration: 5000,
-              },
-            );
-          }, 500);
         }
       }
+
+      setDelta(0);
     }
-  }, []);
+  }, [delta]);
 
   return null;
 }
