1c919631977013cf6d53ba2f0b6cce6870634645
dishwasher-detergent <kenny_bass@outlook.com>
2024-09-04 11:13:32 -0500

    fix z-index issue

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
           <Statistics />
         </div>
-        <nav className="fixed bottom-0 z-[9999] flex w-full flex-none items-center justify-center gap-4 border-t bg-background p-2">
+        <nav className="fixed bottom-0 z-40 flex w-full flex-none items-center justify-center gap-4 border-t bg-background p-2">
           <Drawer>
             <DrawerTrigger asChild>
               <Button size="icon" variant="ghost">
