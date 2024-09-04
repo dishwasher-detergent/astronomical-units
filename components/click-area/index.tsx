f9687baf58ac0b147534adfaecd9d3edecd383e8
Kenneth Bass <kenny_bass@outlook.com>
2024-09-03 20:51:31 -0500

    update text

--- components/click-area/index.tsx
+++ components/click-area/index.tsx
@@ -19,15 +19,15 @@ export function ClickArea() {
         setClicks();
         currentTarget.blur();
       }}
-      className="relative h-full w-full flex flex-none flex-col items-center justify-center overflow-hidden rounded-none bg-muted hover:bg-muted border-b"
+      className="relative flex h-full w-full flex-none flex-col items-center justify-center overflow-hidden rounded-none border-b bg-muted hover:bg-muted"
     >
-      <div className="z-10 text-muted-foreground dark:text-primary flex items-center flex-col">
+      <div className="z-10 flex flex-col items-center text-primary">
         <LucideEarth className="mb-2 size-8" />
         <span>Tap Here</span>
       </div>
-      <div className="animate-blob absolute -left-4 top-0 h-72 w-72 rounded-full bg-green-300 opacity-40 blur-xl filter dark:bg-green-500"></div>
-      <div className="animate-blob animation-delay-2000 absolute -right-4 top-0 h-72 w-72 rounded-full bg-yellow-300 opacity-40 blur-xl filter dark:bg-yellow-500"></div>
-      <div className="animate-blob animation-delay-4000 absolute -bottom-8 left-28 h-72 w-72 rounded-full bg-pink-300 opacity-40 blur-xl filter dark:bg-pink-500"></div>
+      <div className="absolute -left-4 top-0 h-72 w-72 animate-blob rounded-full bg-green-300 opacity-40 blur-xl filter dark:bg-green-500"></div>
+      <div className="animation-delay-2000 absolute -right-4 top-0 h-72 w-72 animate-blob rounded-full bg-yellow-300 opacity-40 blur-xl filter dark:bg-yellow-500"></div>
+      <div className="animation-delay-4000 absolute -bottom-8 left-28 h-72 w-72 animate-blob rounded-full bg-pink-300 opacity-40 blur-xl filter dark:bg-pink-500"></div>
     </Button>
   );
 }
