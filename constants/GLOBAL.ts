60b10aa393e2406b467a9bc2f1688809252afcc6
dishwasher-detergent <kenny_bass@outlook.com>
2024-09-04 14:44:16 -0500

    add type?

--- constants/GLOBAL.ts
+++ constants/GLOBAL.ts
@@ -1,6 +1,6 @@
 export const LOCALE = "en-US";
 
-export const NUMBER_OPTIONS = { 
+export const NUMBER_OPTIONS: Intl.NumberFormatOptions = { 
   style: 'decimal', 
   maximumFractionDigits: 2,
   minimumFractionDigits: 2
