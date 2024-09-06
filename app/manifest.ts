94e201c8fc04c22a175bf8a309b98d1a649cc935
Kenneth Bass <kenny_bass@outlook.com>
2024-09-05 21:23:28 -0500

    add splash screens

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,10 +1,10 @@
-import { APP_DESCRIPTION, APP_NAME } from "@/constants/GLOBAL";
+import { APP_DESCRIPTION, APP_NAME, APP_SHORT_NAME } from "@/constants/GLOBAL";
 import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: APP_NAME,
-    short_name: APP_NAME,
+    short_name: APP_SHORT_NAME,
     description: APP_DESCRIPTION,
     icons: [
       {
@@ -19,8 +19,8 @@ export default function manifest(): MetadataRoute.Manifest {
         type: "image/png",
       },
     ],
-    theme_color: "#FFFFFF",
-    background_color: "#FFFFFF",
+    theme_color: "#000",
+    background_color: "#000",
     start_url: "/",
     display: "standalone",
     orientation: "portrait",
--- constants/GLOBAL.ts
+++ constants/GLOBAL.ts
@@ -1,5 +1,6 @@
 export const LOCALE = "en-US";
 export const APP_NAME = "Astronomical Units";
+export const APP_SHORT_NAME = "AU Idle";
 export const APP_DESCRIPTION =
   "Embark on a cosmic journey where you start as a small space explorer gathering resources from nearby asteroids. As you accumulate more energy and materials, expand your fleet, colonize distant planets, and unlock advanced technologies. Set your sights on conquering the galaxy by discovering new star systems, establishing trade routes, and managing interstellar alliances. The universe is vast, and with every click, your empire grows—one astronomical unit at a time!";
 
