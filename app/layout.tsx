8a7bd731b1f1bb513f164913445009ceaba48e3d
Kenneth Bass <kenny_bass@outlook.com>
2024-09-05 20:19:10 -0500

    PWA?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Space_Grotesk } from "next/font/google";
 import { Dev } from "@/providers/jotai-devtools";
 import { ThemeProvider } from "next-themes";
@@ -13,9 +13,49 @@ import { Button } from "@/components/ui/button";
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
+const APP_NAME = "Astronomical Units";
+const APP_DEFAULT_TITLE = "Astronomical Units";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION =
+  "Embark on a cosmic journey where you start as a small space explorer gathering resources from nearby asteroids. As you accumulate more energy and materials, expand your fleet, colonize distant planets, and unlock advanced technologies. Set your sights on conquering the galaxy by discovering new star systems, establishing trade routes, and managing interstellar alliances. The universe is vast, and with every click, your empire grows—one astronomical unit at a time!";
+
 export const metadata: Metadata = {
-  title: "Astronomical Units",
-  description: "Embark on a cosmic journey where you start as a small space explorer gathering resources from nearby asteroids. As you accumulate more energy and materials, expand your fleet, colonize distant planets, and unlock advanced technologies. Set your sights on conquering the galaxy by discovering new star systems, establishing trade routes, and managing interstellar alliances. The universe is vast, and with every click, your empire grows—one astronomical unit at a time!",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
