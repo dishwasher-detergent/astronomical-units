075d8cd4cde1de9cc714df2d15197a4846a6f3f4
Kenneth Bass <kenny_bass@outlook.com>
2024-09-06 12:07:37 -0500

    sticky stats

--- components/statistics/index.tsx
+++ components/statistics/index.tsx
@@ -5,7 +5,7 @@ import { AusPerSecond } from "@/components/statistics/aus-per-second";
 
 export function Statistics() {
   return (
-    <div className="z-10 flex-none space-y-2 border-b bg-background p-4">
+    <div className="z-10 flex-none space-y-2 border-b bg-background p-4 md:sticky md:top-0">
       <ClickTotalCount />
       <AusPerSecond />
     </div>
