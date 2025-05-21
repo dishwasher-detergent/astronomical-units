fe0e88bb6a8c298c713ad0bfb8dcaca092e65480
Kenneth Bass <kenny_bass@outlook.com>
2025-05-20 22:32:01 -0500

    fix for mobile

--- components/statistics/index.tsx
+++ components/statistics/index.tsx
@@ -7,7 +7,7 @@ import { LifetimeIncomeStats } from "@/components/statistics/lifetime-income-sta
 
 export function Statistics() {
   return (
-    <div className="bg-background grid flex-none grid-cols-1 gap-4 space-y-2 p-4 md:grid-cols-4 xl:grid-cols-1">
+    <div className="bg-background grid flex-none grid-cols-2 gap-4 space-y-2 p-4 md:grid-cols-4 xl:grid-cols-1">
       <AusPerSecond />
       <ClickTotalCount />
       <LifetimeIncomeStats />
