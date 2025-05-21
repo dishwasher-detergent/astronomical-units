98e1ca3752f5e35b4fc750da3c09a015c5db3aef
Kenneth Bass <kenny_bass@outlook.com>
2025-05-20 22:28:16 -0500

    fix squash

--- components/statistics/index.tsx
+++ components/statistics/index.tsx
@@ -7,7 +7,7 @@ import { LifetimeIncomeStats } from "@/components/statistics/lifetime-income-sta
 
 export function Statistics() {
   return (
-    <div className="bg-background grid flex-none grid-cols-4 gap-4 space-y-2 p-4 xl:grid-cols-2">
+    <div className="bg-background grid flex-none grid-cols-1 gap-4 space-y-2 p-4 md:grid-cols-4 xl:grid-cols-1">
       <AusPerSecond />
       <ClickTotalCount />
       <LifetimeIncomeStats />
