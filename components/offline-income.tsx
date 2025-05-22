de4910536bb97674e139ca440476c7930c6e809d
Kenneth Bass <kenny_bass@outlook.com>
2025-05-22 18:19:39 -0500

    up offline period

--- components/offline-income.tsx
+++ components/offline-income.tsx
@@ -28,7 +28,7 @@ export function OfflineIncome() {
   const [offlineTime, setOfflineTime] = useState("");
   const [bonusMessage, setBonusMessage] = useState("");
 
-  const MIN_OFFLINE_MS = 1000;
+  const MIN_OFFLINE_MS = 6000;
   const MAX_OFFLINE_MS = 14400000;
 
   const claimOfflineEarnings = () => {
