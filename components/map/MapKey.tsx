8298e4461ad40315bc42275ebbc1496f016e0cbc
Kenneth Bass <kenny_bass@outlook.com>
2025-05-24 18:17:54 -0500

    fix key covering

--- components/map/MapKey.tsx
+++ components/map/MapKey.tsx
@@ -1,6 +1,6 @@
 export function MapKey() {
   return (
-    <div className="bg-muted absolute bottom-0 left-0 z-10 w-full rounded-lg p-4">
+    <div className="absolute bottom-0 left-0 z-10 w-full rounded-lg p-4">
       <p className="mb-1 font-bold">Legend</p>
       <p className="flex items-center gap-2 text-sm">
         <i className="bg-upgrade-1 block size-2 rounded-full" />
